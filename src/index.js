ffefa11d5e66e7ae2973e51d8626a9f5c05cdccd
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-09-12 20:22:51 +0300

    Remove extra `process` method

--- src/index.js
+++ src/index.js
@@ -7,6 +7,4 @@ const prettify = postcss.plugin('postcss-prettify', () => css => {
   if (css.first && css.first.raws) css.first.raws.before = ''
 })
 
-prettify.process = css => postcss([prettify]).process(css)
-
 export default prettify
